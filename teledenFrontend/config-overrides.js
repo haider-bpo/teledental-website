5ba9ddb6e7edfdc3223a0a9580027cc3be8091aa
haider-bpo <haiderdev.rankorbit@gmail.com>
2025-03-27 15:56:43 -0400

    revert changes to fix buid errors

--- teledenFrontend/config-overrides.js
+++ teledenFrontend/config-overrides.js
@@ -1,86 +1,22 @@
 // config-overrides.js
-const webpack = require("webpack");
-const TerserPlugin = require("terser-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
-const CompressionPlugin = require("compression-webpack-plugin");
+const webpack = require('webpack');
 
 module.exports = function override(config, env) {
-  // Fallbacks for polyfills
   config.resolve.fallback = {
     ...config.resolve.fallback,
-    buffer: require.resolve("buffer/"),
-    crypto: require.resolve("crypto-browserify"),
-    stream: require.resolve("stream-browserify"),
-    process: require.resolve("process/browser"),
+    buffer: require.resolve('buffer/'),
+    crypto: require.resolve('crypto-browserify'),
+    stream: require.resolve('stream-browserify'),
+    process: require.resolve('process/browser'), // Add this line
   };
 
   // Add the process polyfill as a plugin
   config.plugins.push(
     new webpack.ProvidePlugin({
-      process: "process/browser",
+      process: 'process/browser',
     })
   );
 
-  // Only apply optimizations in production mode
-  if (env === "production") {
-    // Optimize JavaScript with Terser
-    config.optimization = {
-      ...config.optimization,
-      minimize: true,
-      minimizer: [
-        new TerserPlugin({
-          terserOptions: {
-            parse: {
-              ecma: 8,
-            },
-            compress: {
-              ecma: 5,
-              warnings: false,
-              comparisons: false,
-              inline: 2,
-              drop_console: true,
-            },
-            mangle: {
-              safari10: true,
-            },
-            output: {
-              ecma: 5,
-              comments: false,
-              ascii_only: true,
-            },
-          },
-          parallel: true,
-          extractComments: false,
-        }),
-        new CssMinimizerPlugin(),
-      ],
-      splitChunks: {
-        chunks: "all",
-        name: false,
-        cacheGroups: {
-          vendor: {
-            test: /[\\]node_modules[\\]/,
-            name: "vendors",
-            chunks: "all",
-          },
-        },
-      },
-      runtimeChunk: {
-        name: (entrypoint) => `runtime-${entrypoint.name}`,
-      },
-    };
-
-    // Add compression plugin for gzip
-    config.plugins.push(
-      new CompressionPlugin({
-        algorithm: "gzip",
-        test: /\.(js|css|html|svg)$/,
-        threshold: 10240,
-        minRatio: 0.8,
-      })
-    );
-  }
-
   return config;
 };
+
