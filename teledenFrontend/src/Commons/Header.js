a5ddc6a05b1220f5c87dc8d66088a92ddbdf7dac
talha <haiderdev.rankorbit@gmail.com>
2025-04-12 05:01:30 +0500

    new figma changes completed

--- teledenFrontend/src/Commons/Header.js
+++ teledenFrontend/src/Commons/Header.js
@@ -45,6 +45,7 @@ function Header({ cssClass }) {
         activeTab[i] =
           strArr[i]?.charAt(0)?.toUpperCase() + strArr[i]?.slice(1);
       }
+      console.log("tab is ", activeTab.join(""));
       setActiveTab(activeTab.join(" "));
     }
   }, [pathname]);
@@ -297,7 +298,7 @@ function Header({ cssClass }) {
 
                         <li className="brix---header-nav-list-item-2">
                           <Link
-                            to="/contact"
+                            to="/contact-us"
                             name="contact"
                             className={`brix---header-nav-link-white w-nav-link ${
                               activeTab === "Contact" && "active selected"
@@ -309,11 +310,10 @@ function Header({ cssClass }) {
 
                         <li className="brix---header-nav-list-item-2">
                           <Link
-                            to="/join-virtual-tele-dental-care"
+                            to="/join-us"
                             name="patient"
                             className={`brix---header-nav-link-white w-nav-link ${
-                              activeTab === "Join Virtual Tele Dental Care" &&
-                              "active selected"
+                              activeTab === "JoinUs" && "active selected"
                             }`}
                             // onClick={() => setIsSignUpModalVisible("patient")}
                           >
@@ -322,11 +322,10 @@ function Header({ cssClass }) {
                         </li>
                         <li className="brix---header-nav-list-item-2">
                           <Link
-                            to="/login"
-                            name="patient"
+                            to="/auth"
+                            name="login"
                             className={`brix---header-nav-link-white w-nav-link ${
-                              activeTab === "Join Virtual Tele Dental Care" &&
-                              "active selected"
+                              activeTab === "Login" && "active selected"
                             }`}
                             // onClick={() => setIsSignUpModalVisible("patient")}
                           >
@@ -335,9 +334,9 @@ function Header({ cssClass }) {
                         </li>
 
                         <li className="brix---header-nav-list-item-2">
-                          <button className="w-nav-link btn_blue">
+                          <Link to={'/book-appointment'} className="btn w-nav-link btn_blue">
                             Book Appointment
-                          </button>
+                          </Link>
                         </li>
                         {/* <li class="brix---header-nav-list-item-2 dropdown">
                           <span
